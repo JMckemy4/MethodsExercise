2fd585cba2c2335ee7eb7ac4b6c3e908cf64e321
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-25 11:59:16 -0700

    method changes

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What is my favorite color?");
+            //string color = Console.ReadLine();
+            //Console.WriteLine("What is your favorite animal?");
+            //string animal  = Console.ReadLine();
+            //Console.WriteLine("What is your favorite band?");
+            //string band = Console.ReadLine();
+            //Console.WriteLine($"The character above {name}, has a favorite {color}. His {animal} is his favortie companion. When his favorite {band} makes him a rocker!!");
+            Console.WriteLine(methodex2.Sum(6, 2));
+
+          
+            
         }
     }
 }
---
+++ MethodsExercise/methodex2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class methodex2
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+    }
+}
